97fd2ec18d4f58c3bed2fb5866e696bb090e9459
SynthCyraX <SynthCyraX@gmail.com>
2023-08-23 14:57:05 +0200

    Added functions for toggle

--- src/index.js
+++ src/index.js
@@ -5,14 +5,46 @@ import './style.css';
 import template from './modules/template';
 
 template();
+const p = document.querySelector('p');
+const fetchButton = document.getElementById('fetch');
+const celsiusButton = document.getElementById('celsius');
+const fahrenheitButton = document.getElementById('fahrenheit');
+
 // input the location and receive weatherdata via console
 async function getWeather() {
+  const input = document.querySelector('input').value;
+
   const response = await fetch(
-    'http://api.weatherapi.com/v1/current.json?key=3a25b7e9e04241afbfe75158232208&q=London&aqi=no',
+    `http://api.weatherapi.com/v1/current.json?key=3a25b7e9e04241afbfe75158232208&q=${input}&aqi=no`,
     { mode: 'cors' }
   );
   const weatherData = await response.json();
 
-  console.log(weatherData);
+  p.innerText = `Temperatur in C: ${weatherData.current.temp_c}°`;
+
+  return weatherData;
 }
-getWeather();
+
+function changeTempToCelsius(weatherData) {
+  p.innerText = `Temperatur in C: ${weatherData.current.temp_c}°`;
+}
+
+function changeTempToCelsiusClick() {
+  getWeather().then((weatherData) => {
+    changeTempToCelsius(weatherData);
+  });
+}
+
+function changeTempToFahrenheit(weatherData) {
+  p.innerText = `Temperatur in F: ${weatherData.current.temp_f}°`;
+}
+
+function changeTempToFahrenheitClick() {
+  getWeather().then((weatherData) => {
+    changeTempToFahrenheit(weatherData);
+  });
+}
+
+fetchButton.addEventListener('click', getWeather);
+celsiusButton.addEventListener('click', changeTempToCelsiusClick);
+fahrenheitButton.addEventListener('click', changeTempToFahrenheitClick);

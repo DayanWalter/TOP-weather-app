1370895d035884c6b7077ea2245d85a77a0356bc
SynthCyraX <SynthCyraX@gmail.com>
2023-08-26 06:15:05 +0200

    Added module

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import '@fortawesome/fontawesome-free/css/all.css';
 
 import './style.css';
 
-const p = document.querySelector('p');
+import UI from './modules/UI';
+
 const fetchButton = document.getElementById('fetch');
 const celsiusButton = document.getElementById('celsius');
 const fahrenheitButton = document.getElementById('fahrenheit');
 
-// input the location and receive weatherdata via console
 async function getWeather() {
   const input = document.querySelector('input').value;
 
@@ -17,30 +17,21 @@ async function getWeather() {
     { mode: 'cors' }
   );
   const weatherData = await response.json();
-  changeTempToCelsius(weatherData);
+
+  UI.changeTempToCelsius(weatherData);
+
   return weatherData;
 }
 
-function changeTempToCelsius(weatherData) {
-  p.innerText = `Temperatur in C: ${weatherData.current.temp_c}°`;
-}
-
-function changeTempToCelsiusClick() {
-  getWeather().then((weatherData) => {
-    changeTempToCelsius(weatherData);
-  });
-}
-
-function changeTempToFahrenheit(weatherData) {
-  p.innerText = `Temperatur in F: ${weatherData.current.temp_f}°`;
-}
-
-function changeTempToFahrenheitClick() {
-  getWeather().then((weatherData) => {
-    changeTempToFahrenheit(weatherData);
-  });
-}
-
 fetchButton.addEventListener('click', getWeather);
-celsiusButton.addEventListener('click', changeTempToCelsiusClick);
-fahrenheitButton.addEventListener('click', changeTempToFahrenheitClick);
+celsiusButton.addEventListener('click', () => {
+  getWeather().then((weatherData) => {
+    UI.changeTempToCelsius(weatherData);
+  });
+});
+
+fahrenheitButton.addEventListener('click', () => {
+  getWeather().then((weatherData) => {
+    UI.changeTempToFahrenheit(weatherData);
+  });
+});
--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,1 +1,12 @@
-export default class UI {}
+const p = document.querySelector('p');
+export default class UI {
+  // input the location and receive weatherdata via console
+
+  static changeTempToCelsius(weatherData) {
+    p.innerText = `Temperatur in C: ${weatherData.current.temp_c}°`;
+  }
+
+  static changeTempToFahrenheit(weatherData) {
+    p.innerText = `Temperatur in F: ${weatherData.current.temp_f}°`;
+  }
+}
